f064830f760941657c014e6e38c0a51096fb8302
Brian To <129273682+DumpySlime@users.noreply.github.com>
2025-12-24 00:16:14 +0800

    Update Dashboard.jsx
    remove assignment for now

--- frontend/src/component/Dashboard.jsx
+++ frontend/src/component/Dashboard.jsx
@@ -252,21 +252,21 @@ const allowedSectionsByRole = {
   student: {
     overview: 'Overview',
     materials: 'Materials',
-    assignments: 'Assignments',
+    //assignments: 'Assignments',
     'student-analytics': 'Student Analytics',
 	chatroom: 'AI Chatroom'
   },
   teacher: {
     overview: 'Overview',
     materials: 'Materials',
-    assignments: 'Assignments',
+    //assignments: 'Assignments',
     'student-analytics': 'Student Analytics',
 	chatroom: 'AI Chatroom'
   },
   admin: {
     overview: 'Overview',
     materials: 'Materials',
-    assignments: 'Assignments',
+    //assignments: 'Assignments',
     'student-analytics': 'Student Analytics',
     'add-subject': 'Add Subject',
     'add-user': 'Add User',
@@ -279,7 +279,7 @@ const renderContent = () => {
 	if (activeSection === 'overview') return <Overview activeSection={activeSection} totalStudents={totalStudents} mockMaterials={mockMaterials} mockGeneratedContent={mockGeneratedContent} mockGeneratedVideos={mockGeneratedVideos}/>;
 	if (activeSection === 'materials') return <MaterialViewer activeSection={activeSection} userInfo={props.userInfo} userRole={props.userRole} mockMaterials={mockMaterials}/>;
 	if (activeSection === 'student-analytics') return <StudentAnalytics activeSection={activeSection} mockStudentProgress={mockStudentProgress}/>;
-	if (activeSection === 'assignments') return <Assignment activeSection={activeSection} mockAssignments={mockAssignments} mockStudentProgress={mockStudentProgress}/>;
+	//if (activeSection === 'assignments') return <Assignment activeSection={activeSection} mockAssignments={mockAssignments} mockStudentProgress={mockStudentProgress}/>;
 	if (activeSection === 'add-subject') return <AddSubject activeSection={activeSection} />;
 	if (activeSection === 'add-user') return <AddUser setActiveSection={setActiveSection} />;
 	if (activeSection === 'chatroom') return <AIChatroom />;
