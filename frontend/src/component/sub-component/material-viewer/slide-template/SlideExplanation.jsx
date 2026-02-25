708b88fcddd0882cefe21c344ceb6af01847691c
Brian To <129273682+DumpySlime@users.noreply.github.com>
2026-02-25 23:47:44 +0800

    Update SlideExplanation.jsx

--- frontend/src/component/sub-component/material-viewer/slide-template/SlideExplanation.jsx
+++ frontend/src/component/sub-component/material-viewer/slide-template/SlideExplanation.jsx
@@ -6,7 +6,8 @@ import axios from 'axios'
 /*  slide: {subtitle: conclusion,
             content: conclusion point form,
             slide_type: explanation,
-            page:4} */
+            page: 4,
+            url: video url} */
 function SlideExplanation ({ slide }) {
   // ensure content is in array format
   const contentList = Array.isArray(slide.content) ? slide.content : [slide.content];
@@ -19,6 +20,12 @@ function SlideExplanation ({ slide }) {
           <li key={index}>{item}</li>
         ))}
       </ul>
+      <div className="slide-video" style={{ display: slide.url ? 'block' : 'none' }}>
+        <video controls>
+          <source src={slide.url} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="slide-footer">
         <p>Page {slide.page}</p>
       </div>
